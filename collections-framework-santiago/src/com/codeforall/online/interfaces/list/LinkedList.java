473d7879f8231b6ec3d21ecd3912fce8bd74e9af
AquaTPPT <195988559+AquaTPPT@users.noreply.github.com>
2026-04-06 17:02:05 +0100

    removed comments from LinkedList.

--- collections-framework-santiago/src/com/codeforall/online/interfaces/list/LinkedList.java
+++ collections-framework-santiago/src/com/codeforall/online/interfaces/list/LinkedList.java
@@ -19,21 +19,15 @@ public class LinkedList<T> implements ListInterface<T>, Collection<T> {
 
     @Override
     public boolean add(T el) {
-        //create iterator node, starting at Head
         Node<T> iterator = head;
-        //while iterator get next node != null, get next node
-        //check if el already exists (considering that head is null is necesary to do
         while(iterator.getGetNextNode() != null){
             iterator = iterator.getGetNextNode();
             if(iterator.getData().equals(el)){
                 return false;
             }
         }
-        //create node with el
         Node<T> newNode = new Node<>(el);
-        //when next node is found, set next node to new node
         iterator.setNextNode(newNode);
-        //increments lenght
         lenght++;
         return true;
     }
@@ -44,20 +38,13 @@ public class LinkedList<T> implements ListInterface<T>, Collection<T> {
     }
 
     @Override
-    //deberia ser remove (T el)
     public boolean remove(T el) {
 
-        //declare 2 nodes: A to have a starting point to avoid the node we want to remove
-        //B to compare each other data.
         Node<T> nodeA = head;
         Node<T> nodeB = head.getGetNextNode();
 
-        //while we still have nodes to compare
         while (nodeB != null) {
-
             if(el.equals(nodeB.getData())){
-
-            // we "jump" node B
             nodeA.setNextNode((nodeB.getGetNextNode()));
             lenght--;
             return true;
@@ -65,8 +52,6 @@ public class LinkedList<T> implements ListInterface<T>, Collection<T> {
             nodeA = nodeB;
             nodeB = nodeB.getGetNextNode();
         }
-
-
         return false;
     }
 
@@ -78,15 +63,7 @@ public class LinkedList<T> implements ListInterface<T>, Collection<T> {
 
     @Override
     public boolean contains(T el) {
-
-        //declare interator to check each node starting not from the head but from
-        //1st "index" (list do not have index).
         Node<T> iterator = head.getGetNextNode();
-
-        //while we still have elements to check, we
-        //compare de data of el with the data of iterator
-        //if it founds the same we print, if not we jump to
-        //the following node
         while (iterator != null){
             if(el.equals(iterator.getData())){
                 System.out.println("we found the data");
@@ -111,11 +88,9 @@ public class LinkedList<T> implements ListInterface<T>, Collection<T> {
     @Override
     public T get(int index) {
 
-        //if index >= lenght || if index < 0 || if list is empty
         if (lenght == 0 || index < 0 || index >= lenght){
             return null;
         }
-        //create iterator already pointing "first index"(with data)
         Node<T> iterator = head.getGetNextNode();
         for (int i = 0; i < index; i++){
             iterator = iterator.getGetNextNode();
